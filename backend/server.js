e83d0228a2ae7643567e72df62febd3d7dae255b
Schweitzer <duyashp@gmail.com>
2026-05-01 20:45:24 +0530

    fix port for deployment

--- backend/server.js
+++ backend/server.js
@@ -34,5 +34,5 @@ const PORT = process.env.PORT || 5000;
 
 // start server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
